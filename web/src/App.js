edf69a74387c2ad3a30e6c64c25d777231dccd31
ShirleyGuoGit <guoshuaimail@gmail.com>
2019-06-19 18:47:37 -0700

    add src

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout />
+    </div>
+  );
+}
+
+export default App;
