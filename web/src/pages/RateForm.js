fbb4f5725cf1b808cb130459facadee89a8ce70b
Yulin Chen <leciel36@gmail.com>
2019-06-22 22:46:56 -0700

    Restyle rate form

--- web/src/pages/RateForm.js
+++ web/src/pages/RateForm.js
@@ -1,39 +1,134 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import CardMedia from '@material-ui/core/CardMedia';
+
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Built with love by the '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </Link>
+      {' team.'}
+    </Typography>
+  );
+}
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+    ul: {
+      margin: 0,
+      padding: 0,
+    },
+    li: {
+      listStyle: 'none',
+    },
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(0, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor: theme.palette.grey[200],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  cardButton: {
+    display: "block",
+    textAlign: "initial"
+  },
+}));
+
+const tiers = [
+  {
+    orderId: "123",
+    title: 'Drone',
+    description: ['Super-Fast', '2019/07/01 15:30'],
+    buttonText: '15',
+    price:'15',
+  },
+  {
+    orderId: "456",
+    title: 'Robot',
+    description: ['Fast', '2019/07/01 10:30'],
+    buttonText: '7',
+    price:'7',
+  },
+];
 
 export default function RateForm() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardName" label="Name on card" fullWidth />
+      <CssBaseline />
+      {/* Hero unit */}
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography variant="h7" align="left" color="textSecondary" component="p">
+          <p>From: Lillian Wood</p>
+          <p>555 Palo Verde Rd, San Francisco, CA</p>
+          <p>To: John Smith</p>
+          <p>100 3rd Ave Apt 505, San Francisco, CA</p>
+        </Typography>
+      </Container>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} >
+          {tiers.map(tier => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid item key={tier.orderId} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={12}>
+              <Card onClick={() => {
+                // TODO (yulin: comeback and provide select shipping method callback)
+              }}>
+                <CardActionArea>
+                  <CardHeader
+                    title={tier.title}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <ul>
+                      {tier.description.map(line => (
+                        <Typography component="li" variant="subtitle1" align="center" key={line}>
+                          {line}
+                        </Typography>
+                      ))}
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button fullWidth variant="contained" color="primary">
+                      ${tier.price}
+                    </Button>
+                  </CardActions>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardNumber" label="Card number" fullWidth />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="expDate" label="Expiry date" fullWidth />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
-      </Grid>
+      </Container>
     </React.Fragment>
   );
 }
