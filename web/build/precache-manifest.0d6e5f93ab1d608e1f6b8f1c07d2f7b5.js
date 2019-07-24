5946d1e69a2252de7da2cbb73b09dc2e312a9af0
ShirleyGuoGit <guoshuaimail@gmail.com>
2019-07-24 14:12:01 -0700

    Depolyed to AWS

---
+++ web/build/precache-manifest.0d6e5f93ab1d608e1f6b8f1c07d2f7b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc000c2ec31f279f51b1886779b4395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333dd5bbb32188ecb241",
+    "url": "/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "49b1d0845539ff093125",
+    "url": "/static/js/2.bd62681d.chunk.js"
+  },
+  {
+    "revision": "333dd5bbb32188ecb241",
+    "url": "/static/js/main.a3457765.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "224115850e1779c35a95f8a59006a021",
+    "url": "/static/media/background.22411585.jpg"
+  },
+  {
+    "revision": "0b13953473ac479a6c1820bf284944a6",
+    "url": "/static/media/drone.0b139534.jpg"
+  },
+  {
+    "revision": "59cb38d5a69513cdbe1056ced522f6ae",
+    "url": "/static/media/robot.59cb38d5.jpg"
+  }
+]);
